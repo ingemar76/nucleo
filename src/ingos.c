12333d28357103ed4fd96b3bd03ad9b8fb7f94f8
ingemar <ingemar.hammarstrom@gmail.com>
2017-04-04 22:01:42 +0200

    Added (very) simple message handling

--- src/ingos.c
+++ src/ingos.c
@@ -3,112 +3,178 @@
 
 #include <string.h>
 
-#define MAX_MESSAGES 20
-#define MAX_TASKS 20
-#define MAX_TIMERS 50
+ingos_message* messages;
+ingos_task* tasks;
+ingos_timer* timer_head;
 
-#define NULL_ID 9999
+static int running = 0;
 
-typedef struct {
-  ingos_task_id dst_task_id;
-  uint32_t timer;
-  uint8_t recurring;
-  void* data;
 
-  uint32_t start_time;
-} ingos_timer;
+uint16_t __get_task(uint16_t ID, ingos_task** task) {
+  ingos_task* ptr = tasks;
 
-ingos_message messages[MAX_MESSAGES];
-ingos_task tasks[MAX_TASKS];
-ingos_timer timers[MAX_TIMERS];
+  while (ptr) {
+    if ((ptr->ID == ID)) {
+      *task = ptr;
+      return INGOS_ERR_OK;
+    }
 
-ingos_message msg;
+    ptr = ptr->__next;
+  }
+
+  return INGOS_ERR_INVALID_TASK_ID;
+}
+
+uint8_t __task_exists(uint16_t ID) {
+  ingos_task* tmp;
+  if (__get_task(ID, &tmp) != INGOS_ERR_OK) return 1;
+
+  return 0;
+}
+
+uint16_t __call_handler (ingos_message* message) {
+  ingos_task* ptr;
+  uint16_t ret = __get_task(message->dst_task_id, &ptr);
+  if (ret != INGOS_ERR_OK) return ret;
+
+  ptr->handler(message);
+  return INGOS_ERR_OK;
+}
 
 void init_ingos() {
+  if (running) return;
+
   int i;
-  memset(messages, 0, sizeof(messages));
-  memset(tasks, 0, sizeof(tasks));
-  memset(timers, 0, sizeof(timers));
+  messages = NULL;
+  timer_head = NULL;
+  tasks = NULL;
+}
 
-  for (i = 0; i < MAX_MESSAGES; i++) {
-    messages[i].message_type = INGOS_MESSAGE_NULL;
-  }
-
-  for (i = 0; i < MAX_TASKS; i++) {
-    tasks[i] = NULL;
-  }
-
-  for (i = 0; i < MAX_TIMERS; i++) {
-    timers[i].dst_task_id = NULL_ID;
+void ingos_error() {
+  while (1) {
+    //Eternal loop ....
   }
 }
 
-void __error() {
+void ingos_start() {
+  if (running) return;
 
-}
-
-void start_ingos() {
-  int i;
   while (1) {
 
     /*Timers*/
-    for (i = 0; i < MAX_TIMERS; i++) {
-      if (timers[i].dst_task_id != NULL_ID) {
-        if ((HAL_GetTick() - timers[i].start_time) >= timers[i].timer) {
-          if (timers[i].dst_task_id >= MAX_TASKS) {
-            __error();
-          }
-          msg.message_type = INGOS_MESSAGE_TIMER;
-          msg.data = timers[i].data;
-          //Call message handler
-          tasks[timers[i].dst_task_id](&msg);
 
-          if (timers[i].recurring) {
-            timers[i].start_time += timers[i].timer;
+    ingos_timer* ptr = timer_head;
+    while (ptr) {
+      if ((HAL_GetTick() - ptr->__start_time) >= ptr->time) {
+        uint32_t t0 = HAL_GetTick();
+        //Call message handler
+        uint16_t ret = __call_handler(ptr->message);
+        if (ret != INGOS_ERR_OK) ingos_error();
+
+        if (!ptr->recurring) {
+          //This timer shall be removed
+          if (!ptr->__prev && !ptr->__next) {
+            //Head, no more timers
+            timer_head = NULL;
+          } else if (ptr->__prev && ptr->__next) {
+            //In the middle of the chain
+            ingos_timer* p = ptr->__prev;
+            ingos_timer* n = ptr->__next;
+            p->__next = n;
+            n->__prev = p;
+          } else if(ptr->__prev && ! ptr->__next) {
+            //Last element
+            ingos_timer* p = ptr->__prev;
+            p->__next = NULL;
           } else {
-            //Delete the timer
-            timers[i].dst_task_id = NULL_ID;
+            ingos_error();
           }
+        } else {
+          ptr->__start_time = HAL_GetTick();
         }
       }
+
+      ptr = ptr->__next;
     }
 
+    /*Messages*/
+    while (messages) {
+      //Call message handler
+      uint16_t ret = __call_handler(messages);
+      if (ret != INGOS_ERR_OK) ingos_error();
+
+      //Go to next message
+      messages = messages->__next;
+    }
   }
 }
 
-uint16_t add_ingo_task(ingos_task task, ingos_task_id* task_id) {
-  int i;
-  for (i = 0; i < MAX_TASKS; i++) {
-    if (!tasks[i]) {
-      tasks[i] = task;
-      *task_id = i;
-      return INGOS_ERR_OK;
+uint16_t ingos_add_task(ingos_task* task) {
+  if (!task->handler) return INGOS_ERR_INVALID_HANDLER;
+
+  if (!tasks) {
+    tasks = task;
+    task->ID = 0;
+    task->__next = NULL;
+  } else {
+    ingos_task* ptr = tasks;
+    //Fast forward to last element
+    while (ptr->__next) {}
+
+    ptr->__next = task;
+    task->ID = ptr->ID + 1;
+    task->__next = NULL;
+  }
+
+  return INGOS_ERR_OK;
+}
+
+
+uint16_t ingos_add_timer(ingos_timer* timer) {
+
+  if (!timer) return INGOS_ERR_INVALID_TIMER;
+  if (!timer->message) return INGOS_ERR_INVALID_MESSAGE;
+
+  timer->__start_time = HAL_GetTick();
+  timer->message->message_type = INGOS_MESSAGE_TIMER;
+  timer->__next = NULL;
+  timer->__prev = NULL;
+
+  if(!timer_head) {
+    timer_head = timer;
+    return INGOS_ERR_OK;
+  }
+
+  ingos_timer* ptr = timer_head;
+  while (1) {
+    if (!ptr->__next) {
+      ptr->__next = timer;
+      timer->__prev = ptr;
+      break;
+    } else {
+      ptr = ptr->__next;
     }
   }
 
-  return INGOS_ERR_MAX_TASKS;
+  return INGOS_ERR_OK;
 }
 
-uint16_t add_ingo_timer(ingos_task_id dst_task_id, uint32_t timer, uint8_t recurring, void* data) {
-  int i;
+uint16_t ingos_post_message(ingos_message* message) {
+  if (!message) return INGOS_ERR_INVALID_MESSAGE;
+  if (!message->dst_task_id) return INGOS_ERR_INVALID_TASK_ID;
+  if (!__task_exists(message->dst_task_id)) return INGOS_ERR_INVALID_TASK_ID;
 
-  if (timer == 0) return INGOS_ERR_INVALID_TIME;
-  if (dst_task_id >= MAX_TASKS || !tasks[dst_task_id]) return INGOS_ERR_INVALID_TASK_ID;
+  message->__next = NULL;
+  ingos_message* ptr = messages;
 
-  for (i = 0; i < MAX_TIMERS; i++) {
-    if (timers[i].dst_task_id == NULL_ID) {
-      timers[i].dst_task_id = dst_task_id;
-      timers[i].timer = timer;
-      timers[i].recurring = recurring;
-      timers[i].data = data;
-      timers[i].start_time = HAL_GetTick();
-
-      return INGOS_ERR_OK;
+  if (!ptr) {
+    messages = message;
+  } else {
+    //Forward until we get to last element
+    while (ptr->__next) {
+      ptr = ptr->__next;
     }
+    ptr->__next = message;
   }
-  return INGOS_ERR_MAX_TIMERS;
-}
-
-void post_message(ingos_message message) {
-
+  return INGOS_ERR_OK;
 }
--- src/ingos.h
+++ src/ingos.h
@@ -2,29 +2,54 @@
 
 typedef enum {
   INGOS_ERR_OK,
-  INGOS_ERR_MAX_TASKS,
-  INGOS_ERR_MAX_TIMERS,
+  INGOS_ERR_INVALID_HANDLER,
+  INGOS_ERR_INVALID_MESSAGE,
+  INGOS_ERR_INVALID_TIMER,
   INGOS_ERR_INVALID_TIME,
   INGOS_ERR_INVALID_TASK_ID,
-} INGOS_ERR;
+} INGOS_RET;
 
 typedef enum {
-  INGOS_MESSAGE_NULL,
   INGOS_MESSAGE_PLAIN,
   INGOS_MESSAGE_TIMER,
 } INGOS_MESSAGE_TYPE;
 
-typedef uint16_t ingos_task_id;
-
 typedef struct {
   INGOS_MESSAGE_TYPE message_type;
+  uint16_t dst_task_id;
+  uint16_t src_task_id;
+  uint32_t command;
   void* data;
+
+  //Ingos internals
+  void* __next;
+
 } ingos_message;
 
-typedef void (*ingos_task)(ingos_message*);
+typedef void (*ingos_handler)(ingos_message*);
+
+typedef struct {
+  uint16_t ID;
+  ingos_handler handler;
+
+  //Ingos internals
+  void* __next;
+} ingos_task;
+
+typedef struct {
+  ingos_message* message;
+  uint32_t time;
+  uint8_t recurring;
+
+  //Inogs internals
+  uint32_t __start_time;
+  void* __prev;
+  void* __next;
+} ingos_timer;
 
 void init_ingos();
-void start_ingos();
-uint16_t add_ingo_task(ingos_task task, ingos_task_id* task_id);
-uint16_t add_ingo_timer(ingos_task_id dst_task_id, uint32_t timer, uint8_t recurring, void* data);
-void post_message(ingos_message message);
+void ingos_start();
+void ingos_error();
+uint16_t ingos_add_task(ingos_task* task);
+uint16_t ingos_add_timer(ingos_timer* timer);
+uint16_t ingos_post_message(ingos_message* message);
